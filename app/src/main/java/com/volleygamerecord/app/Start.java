3bfe4c504177259d77e94e968b23c22e0bf0b7c1
Guest <chiachiku@gmail.com>
2014-03-18 10:58:47 +0800

    Second

---
+++ app/src/main/java/com/volleygamerecord/app/Start.java
@@ -0,0 +1,13 @@
+package com.volleygamerecord.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+/**
+ * Created by Jay on 2014/3/18.
+ */
+public class Start  extends Activity {
+}

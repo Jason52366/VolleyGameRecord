16445f03a05a27a9abe23968f212a5660fa674a7
Leon Wang <b99901037@ntu.edu.tw>
2014-03-18 11:17:45 +0800

    create count1,count2,Record3

---
+++ app/src/main/java/com/volleygamerecord/app/Record3.java
@@ -0,0 +1,9 @@
+package com.volleygamerecord.app;
+
+import android.app.Activity;
+
+/**
+ * Created by user on 2014/3/18.
+ */
+public class Record3  extends Activity {
+}
---
+++ app/src/main/java/com/volleygamerecord/app/count2.java
@@ -0,0 +1,9 @@
+package com.volleygamerecord.app;
+
+import android.app.Activity;
+
+/**
+ * Created by user on 2014/3/18.
+ */
+public class count2  extends Activity {
+}

95e67361e31bbfc39fca27e2d84513c2970e3839
Guest <chiachiku@gmail.com>
2014-04-18 11:50:46 +0800

    Team2 Layout

--- app/src/main/java/com/volleygamerecord/app/Team2.java
+++ app/src/main/java/com/volleygamerecord/app/Team2.java
@@ -2,6 +2,8 @@ package com.volleygamerecord.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Jay on 2014/3/18.
@@ -12,6 +14,8 @@ public class Team2 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team2);
 
+        Button teamName = (TextView)findViewById(R.id.textView_team2TeamName);
+        TextView.setOnClickListener(new Button.OnClickListener(){
 
 
     }

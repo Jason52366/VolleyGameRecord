05006d4c52cf99b06e1621b3e4dbfde4dfc2a202
Guest <chiachiku@gmail.com>
2014-04-18 10:35:20 +0800

    Record1

--- app/src/main/java/com/volleygamerecord/app/Record1.java
+++ app/src/main/java/com/volleygamerecord/app/Record1.java
@@ -19,7 +19,10 @@ import java.util.ArrayList;
  */
 public class Record1 extends Activity {
 
-    ArrayList gamePoint = new ArrayList();
+    ArrayList gamePointList = new ArrayList();
+    Integer ourScore = 0 ;
+    Integer rivalScore = 0;
+
 
     //從datacenter拿資料
     String place = DataCenter.getInstance().getStringValue("place");
@@ -35,19 +38,19 @@ public class Record1 extends Activity {
         String aaa = "a";
         String bbb = "b";
 
-        gamePoint.add(aaa);
-        gamePoint.add(bbb);
+        gamePointList.add(0);
+        gamePointList.add(0);
+
+        //結束比賽的按鈕
         Button btn_startManageTeam = (Button)findViewById(R.id.button_record1UploadGame);
         btn_startManageTeam.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-
-                Log.e("UPPPPP", gamePoint.toString());
-
+                Log.e("UPPPPP", gamePointList.toString());
                 ParseObject gameScore = new ParseObject("GameScore");
-                gameScore.put("score", gamePoint);
+                gameScore.put("score", gamePointList);
                 ParseUser user = ParseUser.getCurrentUser();
-                //This retrieves the currently logged in ParseUser with a valid session
+                //把比賽訊息放入parseObject
                 gameScore.put("user", user);
                 gameScore.put("userName", user.getUsername());
                 gameScore.put("gamePlace", place);
@@ -65,16 +68,24 @@ public class Record1 extends Activity {
                         }
                     }
                 });
-
                 Intent 企圖 = new Intent();
                 企圖.setClass(Record1.this, Count1.class);
                 startActivity(企圖);
-
+                Record1.this.finish();
             }
-
         });
 
 
+        Button btn_getPoint = (Button)findViewById(R.id.button_record1GetPoint);
+        btn_getPoint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent 得分切換 = new Intent();
+                得分切換.setClass(Record1.this, Record2.class);
+                startActivity(得分切換);
+                Record1.this.finish();
+            }
+        });
 
 
 

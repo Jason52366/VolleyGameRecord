f3d84c8575a38bbfc2ac485d1d4b0f5791f36db8
Guest <chiachiku@gmail.com>
2014-05-30 11:22:32 +0800

    整理函式

--- app/src/main/java/com/volleygamerecord/app/GameStart2.java
+++ app/src/main/java/com/volleygamerecord/app/GameStart2.java
@@ -38,55 +38,20 @@ public class GameStart2  extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamestart2);
+        SettingListView();
+        getPlayerFromParse();
+        ChoosePlayer();         //ItemClickListner
+        SureBtn();              //Sure_btnListener
+
+
+    }
+
+    private void SettingListView(){
         listInput = (ListView) findViewById(R.id.listView_gamestart2Player);
         infoItems = new ArrayList<PlayerInfo>();
         infoListAdapter = new PlayerInfoAdapter(this, infoItems);
         listInput.setAdapter(infoListAdapter);
-        getPlayerFromParse();
-
-        //選擇要上場的球員
-        listInput.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                    if(!infoItems.get(position).getOnCourt() && choosenPlayer.size() < 8 ){
-                        view.setBackgroundColor(Color.GREEN);
-                        infoItems.get(position).setOnCourt(Boolean.TRUE);
-                        choosenPlayer.add(infoItems.get(position).getNumber());
-                        infoListAdapter.setOccupy(0);
-                    }else if (infoItems.get(position).getOnCourt()){
-                        view.setBackgroundColor(Color.TRANSPARENT);
-                        infoItems.get(position).setOnCourt(Boolean.FALSE);
-                        int i = choosenPlayer.indexOf(infoItems.get(position).getNumber());
-                        choosenPlayer.remove(i);
-                    }else {
-
-
-                }
-
-            }
-        });
-
-        //button_Sure
-        Button btn_gamestart2Sure = (Button)findViewById(R.id.button_gamestart2Sure);
-        btn_gamestart2Sure.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                if ((choosenPlayer.size() >= 6)){
-                    DataCenter.getInstance().setPlayerArray(choosenPlayer);
-                    Intent intent = new Intent();
-                    intent.setClass(GameStart2.this, Record1.class);
-                    startActivity(intent);
-                    onPause();
-                }
-            }
-        });
-        //召喚了datacenter YOOOOOOO
-        //ArrayList abc = DataCenter.getInstance().getTeamArray();
-        //Log.e("!!!!!", abc.toString() );
     }
-
-
 
     private void getPlayerFromParse(){
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Team");
@@ -113,4 +78,43 @@ public class GameStart2  extends Activity {
         });
     }
 
+    private void ChoosePlayer(){
+        listInput.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if(!infoItems.get(position).getOnCourt() && choosenPlayer.size() < 8 ){
+                    view.setBackgroundColor(Color.GREEN);
+                    infoItems.get(position).setOnCourt(Boolean.TRUE);
+                    choosenPlayer.add(infoItems.get(position).getNumber());
+                    infoListAdapter.setOccupy(0);
+                }else if (infoItems.get(position).getOnCourt()){
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                    infoItems.get(position).setOnCourt(Boolean.FALSE);
+                    int i = choosenPlayer.indexOf(infoItems.get(position).getNumber());
+                    choosenPlayer.remove(i);
+                }else {
+
+
+                }
+
+            }
+        });
+    }
+
+    private void SureBtn(){
+        Button btn_gamestart2Sure = (Button)findViewById(R.id.button_gamestart2Sure);
+        btn_gamestart2Sure.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                if ((choosenPlayer.size() >= 6)){
+                    DataCenter.getInstance().setPlayerArray(choosenPlayer);
+                    Intent intent = new Intent();
+                    intent.setClass(GameStart2.this, Record1.class);
+                    startActivity(intent);
+                    onPause();
+                }
+            }
+        });
+    }
 }
